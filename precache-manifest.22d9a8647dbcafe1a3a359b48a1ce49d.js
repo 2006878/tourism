f7fac87a922012942da6dbf2e7fa9a27c045b3f2
Tairone <taironeamaral@bol.com.br>
2023-10-08 14:20:55 -0300

    Updates

---
+++ precache-manifest.22d9a8647dbcafe1a3a359b48a1ce49d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9f30c16397a656cbc3b",
+    "url": "./static/css/main.9dd1eee5.chunk.css"
+  },
+  {
+    "revision": "b9f30c16397a656cbc3b",
+    "url": "./static/js/main.b9f30c16.chunk.js"
+  },
+  {
+    "revision": "d563e0ae0fa57ac3c150",
+    "url": "./static/css/1.38057cb6.chunk.css"
+  },
+  {
+    "revision": "d563e0ae0fa57ac3c150",
+    "url": "./static/js/1.d563e0ae.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1187699ecd58d62ab7fdf5c42d891e85",
+    "url": "./static/media/bg2.1187699e.jpg"
+  },
+  {
+    "revision": "81bea6edb11573d7b6e781334647b883",
+    "url": "./static/media/cachoeira.81bea6ed.jpg"
+  },
+  {
+    "revision": "6af1f940febfb470e6b4e3fd0bc8fa6a",
+    "url": "./static/media/img-card (1).6af1f940.jpg"
+  },
+  {
+    "revision": "8de4852b189f9dd674e031fbead6e5bb",
+    "url": "./static/media/img-card (2).8de4852b.jpg"
+  },
+  {
+    "revision": "49b0a112ffbe37f707a1a785a24d46eb",
+    "url": "./static/media/img-card (3).49b0a112.jpg"
+  },
+  {
+    "revision": "3c00d79b8a4e54b7b782e94e06f73091",
+    "url": "./static/media/img-card (4).3c00d79b.jpg"
+  },
+  {
+    "revision": "79fdb461c738174a7201402a82089437",
+    "url": "./static/media/resort.79fdb461.jpg"
+  },
+  {
+    "revision": "26206feae9e35179141a2e6c25188270",
+    "url": "./static/media/camp.26206fea.jpg"
+  },
+  {
+    "revision": "febae19d6432cfd722a10e104c0a68cd",
+    "url": "./static/media/mountain.febae19d.jpg"
+  },
+  {
+    "revision": "3d63c9d4836635c380210080c579c7e8",
+    "url": "./static/media/gulp.3d63c9d4.jpg"
+  },
+  {
+    "revision": "74a3598fb7f369e371306a587d1fb0c6",
+    "url": "./static/media/img-card (5).74a3598f.jpg"
+  },
+  {
+    "revision": "362b882f8109d244f853993729764df4",
+    "url": "./static/media/img-card (6).362b882f.jpg"
+  },
+  {
+    "revision": "9cbb33d11f2f21d024318d3eb42f0489",
+    "url": "./static/media/img-card (7).9cbb33d1.jpg"
+  },
+  {
+    "revision": "961d14575cd5e2d595b1e2d2e65f99f8",
+    "url": "./static/media/img-card (8).961d1457.jpg"
+  },
+  {
+    "revision": "22d409d8b5686b39fe598665e8005654",
+    "url": "./static/media/img-card (9).22d409d8.jpg"
+  },
+  {
+    "revision": "b79f100ee8d63006320a68ea0f75222d",
+    "url": "./static/media/barobaybay.b79f100e.jpg"
+  },
+  {
+    "revision": "0cf9a246aebd95c3768bfdbf58e8bf95",
+    "url": "./static/media/award.0cf9a246.svg"
+  },
+  {
+    "revision": "171ba61fc43e1aa5851c232315001107",
+    "url": "./static/media/shield.171ba61f.svg"
+  },
+  {
+    "revision": "1d4ab0808ed85d8b7bf52b4a7acb9b11",
+    "url": "./static/media/compass.1d4ab080.svg"
+  },
+  {
+    "revision": "502189aef3b96acbcddedbb54a043638",
+    "url": "./static/media/heart.502189ae.svg"
+  },
+  {
+    "revision": "4e2c0a1d4a1adb1a99a69310e06fbbc7",
+    "url": "./static/media/trending-up.4e2c0a1d.svg"
+  },
+  {
+    "revision": "e82605c7d2dae238c36c9a96e3b46d03",
+    "url": "./static/media/flag.e82605c7.svg"
+  },
+  {
+    "revision": "0f1222c0565a6b56d03c740e23803859",
+    "url": "./static/media/monitor.0f1222c0.svg"
+  },
+  {
+    "revision": "b2f804bda31dee415fc2b1ff7ba9315f",
+    "url": "./static/media/map-pin.b2f804bd.svg"
+  },
+  {
+    "revision": "2b23e08ac21b7ce2aa82dd856d314841",
+    "url": "./static/media/phone.2b23e08a.svg"
+  },
+  {
+    "revision": "20e8982ec7e03638afe27f5c3e897681",
+    "url": "./static/media/mail.20e8982e.svg"
+  },
+  {
+    "revision": "180a6ca60002b6b9a88b1977232f25fb",
+    "url": "./static/media/facebook.180a6ca6.svg"
+  },
+  {
+    "revision": "43449f1c48de00f0ef903fcacf487626",
+    "url": "./static/media/twitter.43449f1c.svg"
+  },
+  {
+    "revision": "beb7135630c8d3725bc3571be62ad56a",
+    "url": "./static/media/linkedin.beb71356.svg"
+  },
+  {
+    "revision": "6cbcb76e85761740a85b040d708c26bb",
+    "url": "./static/media/instagram.6cbcb76e.svg"
+  },
+  {
+    "revision": "6f082f1262d1dc41f0b5e835b40a52dc",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/tourism/precache-manifest.f7f33bf8fbc0705f2e5b531c8890de45.js"
+  "./precache-manifest.22d9a8647dbcafe1a3a359b48a1ce49d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tourism/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
